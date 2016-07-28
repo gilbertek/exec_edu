8d73bfb7e55a6eb4bd1dc2ca905c3d0563931685
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2016-07-28 16:45:29 -0400

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email,
+    presence: true,
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,17 +10,28 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email should be present" do
-    @user.email = "     "
+    @user.email = " "
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = " "
     assert_not @user.valid?
   end
 
   test "first_name should be present" do
-    @user.first_name = "     "
+    @user.first_name = " "
     assert_not @user.valid?
   end
 
   test "last_name should be present" do
-    @user.last_name = "     "
+    @user.last_name = " "
     assert_not @user.valid?
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
   end
 end
