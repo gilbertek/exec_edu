14975cb67d0a1d70f2301a5af16675de6c23a11b
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2016-07-28 17:16:29 -0400

    login controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  root 'welcome#index'
-  get  '/signup',  to: 'users#new'
-  get  '/login',  to: 'sessions#new'
+  get     '/signup',    to: 'users#new'
 
-  resources :users, only: %w(create)
+  get     '/login',     to: 'sessions#new'
+  post    '/login',     to: 'sessions#create'
+  delete  '/logout',    to: 'sessions#destroy'
+  get     'welcome/index'
+  root    'welcome#index'
+
+  resources :users, only: %w(create new)
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
