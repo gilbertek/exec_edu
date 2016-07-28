e25de430b64a3e90dd6f8d12214a739a7a3a87ac
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2016-07-28 16:00:31 -0400

    Authentication module

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
+  get  '/signup',  to: 'users#new'
+  get  '/login',  to: 'sessions#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
