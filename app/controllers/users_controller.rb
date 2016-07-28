5fe86e3f5e5bbe96191f780d804f88ef83eb196d
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2016-07-28 18:18:38 -0400

    fix test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
