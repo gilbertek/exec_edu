7373002838bbd04d7877c0c7755c8e4e3f6146d4
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2016-07-28 17:02:57 -0400

    adding integration test

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   get  '/login',  to: 'sessions#new'
 
+  resources :users, only: %w(create)
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { first_name:  "",
+                                         last_name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                          } }
+    end
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { first_name:  "John",
+                                         last_name:  "Doe",
+                                         email: "user@example.com",
+                                         password:              "password" } }
+    end
+    # follow_redirect!
+    # assert_template 'users/show'
+  end
+end
