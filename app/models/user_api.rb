a2fff9999c7255100ac4db2282485f318d6f1590
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2016-07-29 16:56:45 -0400

    get user

--- app/models/user_api.rb
+++ app/models/user_api.rb
@@ -5,7 +5,7 @@ class UserApi
 
     def authenticate(params:)
       headers = build_auth_headers(params)
-      response = request(body: {}, headers: headers)
+      response = request(options: {}, headers: headers)
 
       if response.success?
         response.parsed_response["token"]
@@ -14,7 +14,24 @@ class UserApi
       end
     end
 
+    def identify(params:)
+      headers = build_identity_headers(params)
+      response = get_user(options: {}, headers: headers)
+      if response.success?
+        response.parsed_response
+      else
+        fail ExecOnlineApiError::ApiCallFailedError
+      end
+    end
+
     private
+
+    def build_identity_headers(params)
+      {}.tap do |headers|
+        headers['X-Partner-Token']  = EXECONLINE_PARTNER_TOKEN
+        headers['X-User-Token']     = params[:token]
+      end
+    end
 
     def build_auth_headers(params)
       {}.tap do |headers|
@@ -24,13 +41,21 @@ class UserApi
       end
     end
 
-    def request(body:, headers:)
+    def get_user(options:, headers:)
+      HTTParty.get(
+        EXECONLINE_API_URL + '/v1/user.json',
+        body: options.to_json,
+        headers: headers
+      )
+    end
+
+    def request(options:, headers:)
       fail ExecOnlineApiError::ApiCredentialsNotSetError unless EXECONLINE_PARTNER_TOKEN.present?
       fail ExecOnlineApiError::ApiUrlNotSetError unless EXECONLINE_API_URL.present?
 
       HTTParty.post(
         EXECONLINE_API_URL + '/v1/tokens.json',
-        body: body.to_json,
+        body: options.to_json,
         headers: headers
       )
     end
--- test/models/user_api_test.rb
+++ test/models/user_api_test.rb
@@ -1,20 +1,27 @@
 require 'test_helper'
 
 class UserApiTest < ActiveSupport::TestCase
+
   test "#authenticate user" do
     VCR.use_cassette('authenticate_api/post-success') do
       @user_token = UserApi.authenticate(params: {email: 'gilberts55@hotmail.com', password: 'test12345'})
     end
-
     assert_not @user_token.nil?
   end
 
   test "#authenticate user with invalid credentials" do
     VCR.use_cassette('authenticate_api/post-failed') do
-      @user_token = UserApi.authenticate(params: {email: 'example@hotmail.com', password: 'password'})
+      assert_raises ExecOnlineApiError::ApiAuthenticationError do
+        @user_token = UserApi.authenticate(params: {email: 'example@hotmail.com', password: 'password'})
+      end
     end
+  end
 
-    byrbug
-    assert_not @user_token.nil?
+  test "#identify user" do
+    VCR.use_cassette('identity_api/get-success') do
+      @user = UserApi.identify(params: { token: 'fvgAUzxp4cmAzrLoCnOBCw' })
+    end
+    assert_not @user["first_name"].nil?
+    assert_not @user["last_name"].nil?
   end
 end
