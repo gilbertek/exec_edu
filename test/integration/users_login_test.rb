d9fa4a9374cc63e9c0326a672860b578cf49600f
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2016-08-03 14:09:00 -0400

    updates

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid login" do
+    get login_path
+    # post login_path, params: { session: { email: "user@example.com", password: "password" } }
+    # assert_redirected_to login_path
+    # assert_equal 'Invalid email/password combination.', flash[:danger]
+  end
+
+  test "valid login" do
+    get login_path
+
+    # post login_path, params: { session: { email: "gilberts55@hotmail.com", password: "test12345" } }
+    # assert_redirected_to user_courses_index_path(current_user)
+  end
 end
